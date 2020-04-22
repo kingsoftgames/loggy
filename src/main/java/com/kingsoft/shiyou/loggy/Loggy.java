c5b3ce99b02870454094cbd1c02de2bee09bc51f
陶爽 <taoshuang@kingsoft.com>
2020-04-22 15:52:50 +0800

    Make java logging and vertx use log4j2.

--- src/main/java/com/kingsoft/shiyou/loggy/Loggy.java
+++ src/main/java/com/kingsoft/shiyou/loggy/Loggy.java
@@ -7,6 +7,16 @@ import io.vertx.core.Launcher;
  */
 public class Loggy extends Launcher {
 
+    static {
+        // Tell java.util.logging to use log4j2
+        // https://logging.apache.org/log4j/2.0/log4j-jul/index.html
+        System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
+
+        // Tell Vert.x to use log4j2 as the logging framework
+        // See: https://vertx.io/docs/vertx-core/java/#_logging
+        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.Log4j2LogDelegateFactory");
+    }
+
     public static void main(String[] args) {
         new Loggy().dispatch(args);
     }
