4509b6963a651e7492c929b52944410b21bd63e0
陶爽 <taoshuang@kingsoft.com>
2020-04-22 13:53:46 +0800

    Implement loggy http service.

---
+++ src/main/java/com/kingsoft/shiyou/uploader/s3/service/LoggyService.java
@@ -0,0 +1,84 @@
+package com.kingsoft.shiyou.uploader.s3.service;
+
+import com.kingsoft.shiyou.uploader.s3.service.api.v1.Api;
+import com.kingsoft.shiyou.uploader.s3.utils.vertx.Async;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.ErrorHandler;
+import io.vertx.ext.web.handler.LoggerFormat;
+import io.vertx.ext.web.handler.LoggerHandler;
+import lombok.extern.log4j.Log4j2;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import static org.apache.logging.log4j.util.Unbox.box;
+
+/**
+ * @author taoshuang on 2020/4/22.
+ */
+@Log4j2
+@Singleton
+public final class LoggyService implements Async {
+
+    private final Router rootRouter;
+    private final Router apiRouter;
+
+    @Inject
+    HttpServer httpServer;
+
+    @Inject
+    Api api;
+
+    @Inject
+    public LoggyService(Vertx vertx) {
+        rootRouter = Router.router(vertx);
+        apiRouter = Router.router(vertx);
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) {
+        registerRoutes();
+        startHttpServer(startPromise);
+    }
+
+    @Override
+    public void stop(Promise<Void> stopPromise) {
+        stopHttpServer(stopPromise);
+    }
+
+    private void registerRoutes() {
+        registerDefaultRoute();
+        registerApiRoutes();
+    }
+
+    private void registerDefaultRoute() {
+        var route = rootRouter.route();
+        route.handler(BodyHandler.create());
+        route.handler(LoggerHandler.create(false, LoggerFormat.SHORT));
+        route.failureHandler(ErrorHandler.create(true));
+    }
+
+    private void registerApiRoutes() {
+        api.registerRoutes(apiRouter);
+        rootRouter.mountSubRouter("/v1", apiRouter);
+    }
+
+    private void startHttpServer(Promise<Void> promise) {
+        httpServer.requestHandler(rootRouter).listen(ar -> {
+            if (ar.succeeded()) {
+                log.info("Started listening at http://localhost:{}", box(httpServer.actualPort()));
+                promise.complete();
+            } else {
+                promise.fail(ar.cause());
+            }
+        });
+    }
+
+    private void stopHttpServer(Promise<Void> promise) {
+        httpServer.close(promise);
+    }
+}
---
+++ src/main/java/com/kingsoft/shiyou/uploader/s3/service/api/v1/Api.java
@@ -0,0 +1,21 @@
+package com.kingsoft.shiyou.uploader.s3.service.api.v1;
+
+import io.vertx.ext.web.Router;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * @author taoshuang on 2020/4/22.
+ */
+@Singleton
+public final class Api {
+
+    @Inject
+    public Api() {
+    }
+
+    public void registerRoutes(Router router) {
+
+    }
+}
