177a2e1299ba9297cb6a0c45ca407854f4684e7f
陶爽 <taoshuang@kingsoft.com>
2020-04-23 17:28:24 +0800

    Add vertx sdk client for awssdk.

---
+++ src/main/java/com/kingsoft/shiyou/loggy/utils/awssdk/VertxNioAsyncHttpClient.java
@@ -0,0 +1,95 @@
+package com.kingsoft.shiyou.loggy.utils.awssdk;
+
+import com.kingsoft.shiyou.loggy.utils.awssdk.converters.MethodConverter;
+import com.kingsoft.shiyou.loggy.utils.awssdk.reactivestreams.HttpClientRequestSubscriber;
+import com.kingsoft.shiyou.loggy.utils.awssdk.reactivestreams.ReadStreamPublisher;
+import io.vertx.core.Context;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.http.SdkHttpFullResponse;
+import software.amazon.awssdk.http.SdkHttpRequest;
+import software.amazon.awssdk.http.SdkHttpResponse;
+import software.amazon.awssdk.http.async.AsyncExecuteRequest;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
+import software.amazon.awssdk.http.async.SdkAsyncHttpResponseHandler;
+import software.amazon.awssdk.http.async.SdkHttpContentPublisher;
+
+import java.util.concurrent.CompletableFuture;
+
+public class VertxNioAsyncHttpClient implements SdkAsyncHttpClient {
+
+    private final Logger LOG = LoggerFactory.getLogger(VertxNioAsyncHttpClient.class);
+
+    private final Context context;
+    private final HttpClient client;
+
+    public VertxNioAsyncHttpClient(Context context) {
+        this.context = context;
+        this.client = createVertxHttpClient(context.owner());
+    }
+
+    private static HttpClient createVertxHttpClient(Vertx vertx) {
+        HttpClientOptions options = new HttpClientOptions()
+            .setSsl(true)
+            .setKeepAlive(true);
+        return vertx.createHttpClient(options);
+    }
+
+    @Override
+    public CompletableFuture<Void> execute(AsyncExecuteRequest asyncExecuteRequest) {
+        final CompletableFuture<Void> fut = new CompletableFuture<>();
+        if (Context.isOnEventLoopThread()) {
+            executeOnContext(asyncExecuteRequest, fut);
+        } else {
+            context.runOnContext(v -> executeOnContext(asyncExecuteRequest, fut));
+        }
+        return fut;
+    }
+
+    private void executeOnContext(AsyncExecuteRequest asyncExecuteRequest, CompletableFuture<Void> fut) {
+        final SdkHttpRequest request = asyncExecuteRequest.request();
+        final SdkAsyncHttpResponseHandler responseHandler = asyncExecuteRequest.responseHandler();
+
+        final HttpMethod method = MethodConverter.awsToVertx(request.method());
+        final RequestOptions options = getRequestOptions(request);
+        final HttpClientRequest vRequest = client.request(method, options).setFollowRedirects(true);
+        request.headers().forEach((headerName, headerValues) -> {
+            vRequest.putHeader(headerName, String.join(",", headerValues));
+        });
+        vRequest.exceptionHandler(error -> {
+            responseHandler.onError(error);
+            fut.completeExceptionally(error);
+        });
+        vRequest.handler(vResponse -> {
+            final SdkHttpFullResponse.Builder builder = SdkHttpResponse.builder()
+                    .statusCode(vResponse.statusCode())
+                    .statusText(vResponse.statusMessage());
+            vResponse.headers().forEach(e ->
+                    builder.appendHeader(e.getKey(), e.getValue())
+            );
+            responseHandler.onHeaders(builder.build());
+            responseHandler.onStream(new ReadStreamPublisher<>(vResponse, fut));
+        });
+        final SdkHttpContentPublisher publisher = asyncExecuteRequest.requestContentPublisher();
+        if (publisher != null) {
+            publisher.subscribe(new HttpClientRequestSubscriber(vRequest));
+        } else {
+            vRequest.end();
+        }
+    }
+
+    private static RequestOptions getRequestOptions(SdkHttpRequest request) {
+        return new RequestOptions()
+            .setHost(request.host())
+            .setPort(request.port())
+            .setURI(request.encodedPath())
+            .setSsl("https".equals(request.protocol()));
+    }
+
+    @Override
+    public void close() {
+        client.close();
+    }
+}
